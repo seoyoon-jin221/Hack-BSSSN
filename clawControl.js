ba2cd71f20ce73b5cc9bd2270658aa252cef76a1
Nathakin Leepreecha <iamnathan13579@gmail.com>
2019-09-21 10:22:01 -0700

    add claw buttons

--- clawControl.js
+++ clawControl.js
@@ -36,11 +36,31 @@ function clawKnob() {
     b.className = "btn btn-outline-secondary";
     b.innerText = "Send Claw";
     elem.appendChild(b);
+
     var parent = document.getElementById('controlsView');
     parent.appendChild(elem);
     b.addEventListener('touchstart', function() {
         setClaw(clawValue);
     });
+
+    var closeClawButton = document.createElement("button");
+    closeClawButton.className = "btn btn-outline-secondary";
+    closeClawButton.innerText = "Close Claw Completely";
+    elem.appendChild(closeClawButton);
+    
+    var openClawButton = document.createElement("button");
+    openClawButton.className = "btn btn-outline-secondary";
+    openClawButton.innerText = "Open Claw";
+    elem.appendChild(closeClawButton);
+    
+    //step closeclawbubtton
+    var stepCloseClawButton = document.createElement("button");
+    stepCloseClawButton.className = "btn btn-outline-secondary";
+    stepCloseClawButton.innerText = "Step Close Claw";
+    elem.appendChild(stepCloseClawButton);
+
+    var parent = document.getElementById('controlsView');
+    parent.appendChild(elem);
 }
 
 
